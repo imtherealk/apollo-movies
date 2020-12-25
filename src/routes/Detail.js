9bb5cefa53403c37cb94c67a1478b5a80601574b
Nayeong <imtherealk@gmail.com>
2020-12-25 21:40:09 +0900

    Apply optional chaining & Add suggestions

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import styled from "styled-components";
+
+import Movie from "../components/Movie";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -12,6 +14,11 @@ const GET_MOVIE = gql`
       description_intro
       language
       rating
+      genres
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
     }
   }
 `;
@@ -24,6 +31,7 @@ const Container = styled.div`
   justify-content: space-around;
   align-items: center;
   color: white;
+  /* padding: 10px; */
 `;
 
 const Column = styled.div`
@@ -38,6 +46,7 @@ const Title = styled.h1`
 
 const Subtitle = styled.h4`
   font-size: 35px;
+  margin-top: 10px;
   margin-bottom: 10px;
 `;
 
@@ -54,27 +63,68 @@ const Poster = styled.div`
   background-position: center center;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 15px;
+  padding: 10px;
+  position: relative;
+`;
+
+const Divider = styled.hr`
+  border-top: 3px white;
+  margin-top: 50px;
+`;
+const Genres = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 10px;
+`;
+const Genre = styled.li`
+  list-style-position: inside;
+  border: 1px solid white;
+  border-radius: 5px;
+  padding: 3px;
+  margin-right: 10px;
+  font-size: 20px;
+  border-style: solid;
+  background-color: white;
+  color: #2f2f2f;
+`;
 export default () => {
   const { id } = useParams();
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: +id },
   });
+
   return (
     <Container>
       <Column>
         <Title>{loading ? "Loading..." : data.movie.title}</Title>
-        {!loading && data.movie && (
-          <Fragment>
-            <Subtitle>
-              {!loading && data.movie.language} · {data.movie.rating}
-            </Subtitle>
-            <Description>
-              {!loading && data.movie.description_intro}
-            </Description>
-          </Fragment>
-        )}
+        <Subtitle>
+          {data?.movie?.language} · {data?.movie?.rating}
+        </Subtitle>
+        <Genres>
+          {data?.movie?.genres?.map((genre) => (
+            <Genre>{genre}</Genre>
+          ))}
+        </Genres>
+        <Description>{data?.movie?.description_intro}</Description>
+
+        <Divider />
+        <Subtitle>suggestions for you</Subtitle>
+        <Movies>
+          {data?.suggestions?.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              bg={movie.medium_cover_image}
+            ></Movie>
+          ))}
+        </Movies>
       </Column>
-      {data && data.movie && <Poster bg={data.movie.medium_cover_image} />}
+      <Poster bg={data?.movie?.medium_cover_image} />
     </Container>
   );
 };
